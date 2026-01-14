7ddf7f602eafbd8949ccc2101fd0d637db70f289
synthh <sobrepenacarliannoel@gmail.com>
2026-01-14 22:06:28 +0800

    CREATED AuthController.cs and ADDED GetUserById Function

---
+++ backend/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using TaskManager.Data;
+using TaskManager.DTOs;
+
+namespace task_manager_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET USER BY ID END POINT
+        [HttpGet("user{id}")]
+        public async Task<IActionResult> GetUserById([FromRoute] int id)
+        {
+            // CHECK IF USER EXISTS
+            var user = await _context.Users.FindAsync(id);
+
+            // RETURNS 404 IF USER DOES NOT EXIST
+            if (user is null) return NotFound("User does not exist.");
+
+            // RETURNS 200 OK IF USER EXISTS
+            return Ok(user);
+        }
+    }
+}
