1836b3ba0e6034e3b90ecd9a7e257342dffb220f
synthh <sobrepenacarliannoel@gmail.com>
2026-01-15 13:48:30 +0800

    ADDED Register User Function in Auth Controller

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 
 using TaskManager.Data;
 using TaskManager.DTOs;
+using TaskManager.Services;
+using TaskManager.Utility;
+using TaskManager.Models;
+using System.Diagnostics;
 
 namespace task_manager_api.Controllers
 {
@@ -11,10 +15,12 @@ namespace task_manager_api.Controllers
     public class AuthController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private readonly JWTService _jwtService;
 
-        public AuthController(ApplicationDbContext context)
+        public AuthController(ApplicationDbContext context, JWTService jwtService)
         {
             _context = context;
+            _jwtService = jwtService;
         }
 
         // GET USER BY ID END POINT
@@ -29,6 +35,41 @@ namespace task_manager_api.Controllers
 
             // RETURNS 200 OK IF USER EXISTS
             return Ok(user);
+        }
+
+        // REGISTER USER ENDPOINT
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            Debug.WriteLine(request);
+            // CHECK IF EMAIL IS ALREADY REGISTERED
+            var isExist = _context.Users.Any(u => u.Email == request.Email);
+
+            // RETURNS 409 CONFLICT IF EMAIL IS ALREADY REGISTERED
+            if (isExist) return Conflict("Email is already registered.");
+
+            // CREATE NEW USER
+            var user = new User
+            {
+                Email = request.Email,
+                PasswordHash = PasswordHasher.HashPassword(request.Password)
+            };
+
+            // ADDS NEW USER TO CONTEXT
+            _context.Users.Add(user);
+
+            // SAVES CHANGES TO DATABASE
+            await _context.SaveChangesAsync();
+
+            // GENERATES JWT TOKEN
+            var token = _jwtService.GenerateToken(user);
+
+            // RETURNS 201 USER REGISTERED SUCCESSFULLY
+            return CreatedAtAction(
+                nameof(GetUserById),
+                new { Id = user.Id },
+                new RegisterResponse(user.Id, user.Email, token)
+            );
         }
     }
 }
